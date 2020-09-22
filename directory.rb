f79ef0968f341eaff14d39f68a23e57d5e0930a5
Krzysztof Jankowski <k-j@xl.wp.pl>
2020-09-22 15:24:44 +0100

    add interactive menu after refactoring

--- directory.rb
+++ directory.rb
@@ -12,23 +12,21 @@
 #   {name: "Joffrey Baratheon", cohort: :november},
 #   {name: "Norman Bates", cohort: :november}
 # ]
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   #while the name is not empty,  repeat this code
   while !name.empty? do
     #add students hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
 end
 
 def print_header
@@ -36,37 +34,48 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    #input the students
+    students = input_students
+  when "2"
+    # show the students
+    show_students
+  when "9"
+    exit #this will cause the program to terminate
+  else
+      puts "I don't know what you meant, try again"
+  end
 end
 
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-    when "1"
-      #input the students
-      students = input_students
-    when "2"
-      # show the students
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit #this will cause the program to terminate
-    else
-        puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
+
 interactive_menu
