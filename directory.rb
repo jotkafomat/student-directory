49c2030570dcd0f7db8e7c1f5f76d54ce3d9217a
Krzysztof Jankowski <k-j@xl.wp.pl>
2020-09-22 15:11:25 +0100

    add interactive menu

--- directory.rb
+++ directory.rb
@@ -22,7 +22,7 @@ def input_students
   #while the name is not empty,  repeat this code
   while !name.empty? do
     #add students hash to the array
-    students << {name: name, cohort: :novemer}
+    students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
@@ -45,7 +45,28 @@ end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
-students = input_students
-print_header
-print(students)
-print_footer(students)
+
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      #input the students
+      students = input_students
+    when "2"
+      # show the students
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit #this will cause the program to terminate
+    else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
+interactive_menu
