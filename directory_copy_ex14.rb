905671859f27bff845d36c189908fd16d4ee6111
Krzysztof Jankowski <k-j@xl.wp.pl>
2020-09-23 09:21:23 +0100

    exercise14(5) refactor code using block to save file

--- directory_copy_ex14.rb
+++ directory_copy_ex14.rb
@@ -96,14 +96,14 @@ def save_students
     filename = "students.csv"
   end
   #open the file for writing
-  file = File.open(filename, "w")
+  file = File.open(filename, "w") do |file|
   #iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
 end
 
 def load_students(filename = "students.csv")
